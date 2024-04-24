eaad05447d3808e221b3f743d4930776d00faaf8
Chichi0430 <a109221258@mail.shu.edu.tw>
2024-04-24 22:58:17 +0800

    0424

--- RaddioButton1/app/src/main/java/com/example/raddiobutton1/MainActivity.java
+++ RaddioButton1/app/src/main/java/com/example/raddiobutton1/MainActivity.java
@@ -8,8 +8,11 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = (Button) findViewById(R.id.button);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,26 +28,51 @@ public class MainActivity extends AppCompatActivity {
 
                 RadioButton boy =(RadioButton) findViewById(R.id.rdbBoy);
                 RadioButton girl=(RadioButton) findViewById(R.id.rdbGirl);
+                EditText quantityEditText = (EditText) findViewById(R.id.editTextQuantity);
+
+                double ticketPrice = 0.0;
+
+
                 if (boy.isChecked())
                     outputStr += "男生\n";
                 else if (girl.isChecked())
                     outputStr +="女生\n";
                 RadioGroup type = (RadioGroup) findViewById(R.id.rgType);
-               /* switch (type.getCheckedRadioButtonId()){
-                    case R.id.rdbAdult:
-                        outputStr += "全票\n";
-                        break;
-                }*/
+
+
                 if (type.getCheckedRadioButtonId() == R.id.rdbAdult)
-                    outputStr += "全票$500\n";
+                    outputStr += "全票\n";
+
                 else if (type.getCheckedRadioButtonId() == R.id.rdbChild)
-                    outputStr += "兒童票$250\n";
+                    outputStr += "兒童票\n";
                 else
-                    outputStr += "學生票$400\n";
+                    outputStr += "學生票\n";
+
+
+                if (type.getCheckedRadioButtonId() == R.id.rdbAdult)
+                    ticketPrice = 500;
+                else if (type.getCheckedRadioButtonId() == R.id.rdbChild)
+                    ticketPrice = 250;
+                else
+                    ticketPrice = 400;
 
                 TextView output = (TextView) findViewById(R.id.lblOutput);
                 output.setText(outputStr);
+                String quantityStr = quantityEditText.getText().toString();
+                if (!quantityStr.isEmpty()) {
+                    int quantity = Integer.parseInt(quantityStr);
+                    int totalPrice = (int)(ticketPrice * quantity);
+                    outputStr += "張數：" + quantity + "\n";
+                    outputStr += "總價：" + totalPrice + " 元";
+                } else {
+                    outputStr += "請輸入張數";
+                }
+
+                output.setText(outputStr);
+
+
             }
         });
+
     }
 }
