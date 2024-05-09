d639715f6685a7e591c0d165246343a68cc2fdd7
Chichi0430 <a109221258@mail.shu.edu.tw>
2024-05-09 18:00:37 +0800

    0509

--- 0509/ListViewDemo/app/src/main/java/com/example/listviewdemo/MainActivity.java
+++ 0509/ListViewDemo/app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,13 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cities = getResources().getStringArray(R.array.cities);
+        ArrayAdapter<String> cities = new ArrayAdapter<>(this
+        ,android.R.layout.simple_dropdown_item_1line);
+
         lv = (ListView)findViewById(R.id.listview);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 TextView output = (TextView) findViewById(R.id.lblOutput);
-                output.setText("你是在： "+cities[position]);
+                String cities = lv.getSelectedItem().toString();
+                output.setText("你是在： "+ cities);
             }
         });
     }
