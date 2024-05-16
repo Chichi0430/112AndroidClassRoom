55f024154b4e0f09c48fdff44e188cd3f32a6d33
Chichi0430 <a109221258@mail.shu.edu.tw>
2024-05-16 15:36:12 +0800

    0516

--- 0509/SpinnerAdapter/app/src/main/java/com/example/spinneradapter/MainActivity.java
+++ 0509/SpinnerAdapter/app/src/main/java/com/example/spinneradapter/MainActivity.java
@@ -7,10 +7,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.TextView;
-
-import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
     private String[] courses, desserts;
@@ -27,22 +24,33 @@ public class MainActivity extends AppCompatActivity {
         spDesserts = (Spinner) findViewById(R.id.spinner2);
 
         ArrayAdapter<String> adpCourese = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item,courses);
+                android.R.layout.simple_spinner_item, courses);
         ArrayAdapter<String> adpDesserts = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_dropdown_item,desserts);
+                android.R.layout.simple_spinner_dropdown_item, desserts);
 
         spCourses.setAdapter(adpCourese);
         spDesserts.setAdapter(adpDesserts);
 
-        Button btnConfitm = (Button) findViewById(R.id.button);
+        Button btnConfitm = (Button) findViewById(R.id.btnModify);
         btnConfitm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String course = spCourses.getSelectedItem().toString();
                 String dessert = spDesserts.getSelectedItem().toString();
                 TextView output = (TextView) findViewById(R.id.lblOutput);
-                output.setText("主餐： "+course+"\n甜點： " +dessert);
+                output.setText("主餐： " + course + "\n甜點： " + dessert);
             }
         });
+
+        Button btnModify = (Button) findViewById(R.id.btnModify);
+        btnModify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                desserts[4] = "草莓蛋糕";
+                adpDesserts.notifyDataSetChanged();
+            }
+
+        });
+
     }
 }
