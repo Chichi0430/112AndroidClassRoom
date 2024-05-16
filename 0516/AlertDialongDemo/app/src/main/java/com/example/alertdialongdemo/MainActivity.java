599eb0210ad90ff097d1470481dab91aadfd4110
Chichi0430 <a109221258@mail.shu.edu.tw>
2024-05-16 17:09:10 +0800

    0516

--- 0516/AlertDialongDemo/app/src/main/java/com/example/alertdialongdemo/MainActivity.java
+++ 0516/AlertDialongDemo/app/src/main/java/com/example/alertdialongdemo/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.alertdialongdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnAbout = (Button) findViewById(R.id.btnAbout);
+        btnAbout.setOnClickListener( v -> {
+            new AlertDialog.Builder(this)
+                    .setTitle("關於這本書")
+                    .setMessage("Android程式設計與應用\n作者：陳會安\n教師：恰克虎")
+                    .setCancelable(true)
+                    .setPositiveButton("確定",null)
+                    .show();
+        });
     }
 }
