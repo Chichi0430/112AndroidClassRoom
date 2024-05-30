e19f808dd1c6eb077a84bcb6550c6ed5aee92942
Chichi0430 <a109221258@mail.shu.edu.tw>
2024-05-30 17:04:48 +0800

    0530

---
+++ 0530/MyDemo/app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application") //包含複雜編譯
+    id("com.google.gms.google-services") //把專案編譯方式改變
+}
+
+android {
+    namespace = "com.example.mydemo"
+    compileSdk = 34
+
+    defaultConfig {
+
+        applicationId = "com.example.mydemo"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0")) //宣告 使用FirebaseDemo的東西
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
